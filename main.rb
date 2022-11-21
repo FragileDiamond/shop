037ff9609e6de250ce00c5bdd63dd3e941abad6b
FragileDiamond <117909167+FragileDiamond@users.noreply.github.com>
2022-11-21 17:25:25 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-require_relative '../shop/lib/collection'
-require_relative '../shop/lib/cart'
+require_relative '/lib/collection'
+require_relative '/lib/cart'
 
 products = ProductCollection.from_dir(File.dirname(__FILE__) + '/data')
 products.sort!(by: :price, order: :asc)
@@ -10,7 +10,7 @@ loop do
   puts products
   puts '0. Выход'
   print '> '
-  chosen_number = gets.chomp
+  chosen_number = gets.to_i
   break if chosen_number == '0'
 
   chosen_product = products.product_by_index(chosen_number.to_i)
@@ -22,17 +22,17 @@ loop do
 
   puts <<-OUTPUT
 
-Вы выбрали #{chosen_product}
+    Вы выбрали #{chosen_product}
 
-Всего товаров на сумму: #{cart.sum} руб.
+    Всего товаров на сумму: #{cart.sum} руб.
 
   OUTPUT
-
 end
+
 puts <<-CHECK
-Вы купили:
+  Вы купили:
 
-#{cart}
+  #{cart}
 
-С Вас — #{cart.sum} руб. Спасибо за покупки!
+  С Вас — #{cart.sum} руб. Спасибо за покупки!
 CHECK
